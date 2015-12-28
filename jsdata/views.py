2d0bbd4daedc1f43fbc358881c9adbe4585756d8
Luke Hodkinson <furious-luke@gmail.com>
2015-12-29 09:42:03 +1100

    Helper to automatically generate a DRF API.

--- jsdata/views.py
+++ jsdata/views.py
@@ -1,3 +1,9 @@
+import re
+
+
+url_prog = re.compile(r'\^([a-z/]+)(\(\?P<pk>\[\^/\.\]\+\)/)?\$')
+
+
 class JSDataViewMixin(object):
 
     def get_context_data(self, **kwargs):
@@ -7,3 +13,27 @@ class JSDataViewMixin(object):
 
     def get_jsdata(self, **kwargs):
         return kwargs
+
+
+class DRFViewMixin(JSDataViewMixin):
+
+    def get_jsdata(self, **kwargs):
+        data = {'api': self.get_api()}
+        data.update(kwargs)
+        return data
+
+    def get_api(self, **kwargs):
+        api = {}
+        router = self.get_router()
+        for url in router.urls:
+            match = url_prog.match(url.regex.pattern)
+            if not match:
+                continue
+            api[url.name] = self.api_prefix + match.group(1)
+            if match.group(2):
+                api[url.name] += '<pk>/'
+        api.update(kwargs)
+        return api
+
+    def get_router(self):
+        raise NotImplemented
