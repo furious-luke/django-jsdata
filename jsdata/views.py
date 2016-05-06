308e87c44d55813e1e0fe9f2deb2b3ad85beadec
Luke Hodkinson <furious.luke@gmail.com>
2016-05-06 12:26:09 +1000

    Allow for multiple routers (even though this is probably never going to be used).

--- jsdata/views.py
+++ jsdata/views.py
@@ -24,14 +24,17 @@ class DRFViewMixin(JSDataViewMixin):
 
     def get_api(self, **kwargs):
         api = {}
-        router = self.get_router()
-        for url in router.urls:
-            match = url_prog.match(url.regex.pattern)
-            if not match:
-                continue
-            api[url.name] = self.api_prefix + match.group(1)
-            if match.group(2):
-                api[url.name] += '<pk>/'
+        routers = self.get_router()
+        if not isinstance(routers, list):
+            routers = [routers]
+        for router in routers:
+            for url in router.urls:
+                match = url_prog.match(url.regex.pattern)
+                if not match:
+                    continue
+                api[url.name] = self.api_prefix + match.group(1)
+                if match.group(2):
+                    api[url.name] += '<pk>/'
         api.update(kwargs)
         return api
 
